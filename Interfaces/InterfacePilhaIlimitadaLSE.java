46ecb911085201a5b942c97f05527fd7333df62a
Jeconias Santos <jeconiass2009@hotmail.com>
2019-03-09 16:48:01 -0300

    Interface para a classe de gerenciamendo da LL

---
+++ Interfaces/InterfacePilhaIlimitadaLSE.java
@@ -0,0 +1,12 @@
+package interfaces;
+import exceptions.PilhaVaziaException;
+
+public interface InterfacePilhaIlimitadaLSE {
+
+    public void push(int value);
+    public int pop() throws PilhaVaziaException;
+    public int top() throws PilhaVaziaException;
+    public int size();
+    public boolean isEmpty();
+
+} 
