091d8d2749c463af5ddcb8d20161797c48a6f645
Jeconias Santos <jeconiass2009@hotmail.com>
2019-03-09 21:01:35 -0300

    Fila com array (FIFO)

---
+++ Exceptions/FilaVaziaException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class FilaVaziaException extends Exception
+{
+  private static final long serialVersionUID = 4L;
+  
+  public FilaVaziaException(String msg){
+    super(msg);
+  }
+
+}
--- Main.java
+++ Main.java
@@ -7,29 +7,28 @@ public class Main {
   {
 
     // FILA
+    FilaIlimitada filaIlimitada = new FilaIlimitada();
     
 
+    try {
+      filaIlimitada.enfileirar(114);
+      filaIlimitada.enfileirar(100);
+      filaIlimitada.enfileirar(241);
+      filaIlimitada.enfileirar(61);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      System.out.println(filaIlimitada.tamanho());
+      System.out.println(filaIlimitada.desenfileirar());
+      System.out.println(filaIlimitada.desenfileirar());
+      System.out.println(filaIlimitada.desenfileirar());
+      System.out.println(filaIlimitada.desenfileirar());
+      
+      
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
 
     // PILHA
-    PilhaIlimitadaLSE pilhaLSE = new PilhaIlimitadaLSE();
+    /*PilhaIlimitadaLSE pilhaLSE = new PilhaIlimitadaLSE();
     pilhaLSE.push(1);
     pilhaLSE.push(10);
     pilhaLSE.push(21);
@@ -43,7 +42,7 @@ public class Main {
       System.out.println(pilhaLSE.pop());
     } catch (PilhaVaziaException e) {
       System.out.println(e.getMessage());
-    }
+    }*/
     
 
 
