8b7441dd6b0c262902511810d764efe09b192752
Jeconias Santos <jeconiass2009@hotmail.com>
2019-10-14 21:57:11 -0300

    Exceptions to Grafo.

---
+++ exceptions/JokerTypeNotFound.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class JokerTypeNotFound extends Exception {
+    private static final long serialVersionUID = 10L;
+
+    public JokerTypeNotFound(String msg) {
+        super(msg);
+    }
+
+}
---
+++ exceptions/TableIndexException.java
@@ -0,0 +1,10 @@
+package exceptions;
+
+public class TableIndexException extends Exception {
+    private static final long serialVersionUID = 9L;
+
+    public TableIndexException(String msg) {
+        super(msg);
+    }
+
+}
