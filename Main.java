933fa5f6119807243fc35d639996ec67e4d2bf74
Jeconias Santos <jeconiass2009@hotmail.com>
2019-10-30 00:14:57 -0300

    Added depth first search algorithm.

--- Main.java
+++ Main.java
@@ -17,29 +17,20 @@ public class Main {
 
     try {
 
-      aa = g.setAresta(1, 2, true);
-      g.setAresta(0, 0, false);
-      g.setAresta(2, 2, false);
-      g.setAresta(3, 1, false);
-      g.setAresta(4, 1, true);
-      g.setAresta(4, 1, false);
-
-      //g.showTable();
-      //System.out.println();
-      // g.removeVertice(a);
-      // g.removeVertice(b);
-      //g.removeAresta(aa);
+      aa = g.setAresta(2, 0, false);
+      g.setAresta(0, 2, false);
+      g.setAresta(0, 1, false);
+      g.setAresta(1, 2, false);
+      g.setAresta(2, 3, true);
 
     } catch (TableIndexException err) {
       System.out.println(err.getMessage());
       System.exit(1);
     }
 
+    System.out.println(g.dfs("2"));
+    System.out.println();
     g.showTable();
-
-    // System.out.println(a.toString());
-    // System.out.println(b.toString());
-    System.out.println(g.arestasToString());
 
   }
 }
